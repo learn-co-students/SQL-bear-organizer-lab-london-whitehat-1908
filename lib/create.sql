fa323cfa7e18ef7a183a1b5c9d01720467ded425
Oliver <olliepar01@gmail.com>
2019-08-15 16:32:10 +0100

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+    id INTEGER NOT NULL ,
+    Name TEXT,
+    Age INTEGER,
+    Gender TEXT, 
+    Color TEXT,
+    temperament TEXT,
+    alive TEXT,
+    PRIMARY KEY (id)
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears
+VALUES (0, "Bob", 20, 'Male', 'red', 'calm', 'yes'),
+(1, 'Tom', 10, 'Male', 'blue', 'calm', "no"),
+(2, 'Tim', 20, "male", 'red', 'calm', 'yes'),
+(3, "bob", 20, "female", 'red', 'calm', 'yes'),
+(4, "bob", 20, "female", 'red', 'calm', 'yes'),
+(5, "bob", 20, "male", 'red', 'calm', 'yes'),
+(6, "bob", 20, "male", 'red', 'calm', 'yes'),
+(7, null, 20, "male", 'red', 'calm', 'yes');
+
