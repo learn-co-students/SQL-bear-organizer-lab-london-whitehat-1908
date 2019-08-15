4a2f585b9bb7ea2fb047b85373f4b0a6d0712cd2
Callum Forbes <forbescallum@gmail.com>
2019-08-15 11:23:30 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES('Mr. Chocolate', 23, 'F', 'brownish', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES('Rowdy', 23, 'F', 'brownish', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES('Tabitha', 23, 'F', 'brownish', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES('Sergeant Brown', 23, 'F', 'brownish', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES('Melissa', 23, 'F', 'dark brown', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES('Grinch', 23, 'F', 'dark brown', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES('Wendy', 23, 'F', 'dark brown', 'docile', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES(NULL, 20, 'M', 'black', 'aggressive', 0);
