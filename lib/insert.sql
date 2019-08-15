dac986a36e95863539e0d649a5287d6c27058a00
jack272hughes <jack272hughes@gmail.com>
2019-08-15 10:31:58 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, gender, alive)
+VALUES
+('Mr. Chocolate', 5, 'Male', true),
+('Rowdy', 11, 'Male', true),
+('Tabitha', 14, 'Female', true),
+('Sergeant Brown', 19, 'Male', true),
+('Melissa', 8, 'Female', true),
+('Grinch', 23, 'Male', true),
+('Wendy', 17, 'Female', true),
+(null, null, null, null);
