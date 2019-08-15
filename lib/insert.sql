9156fb1ba51d90e8591a28d27d336cc1e12ec089
mini-ninja-64 <callumhart@btinternet.com>
2019-08-15 11:08:48 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, "Mr. Chocoalate", 12, "F", "Brown", "Calm", TRUE);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Rowdy", 6, "F", "Brown", "Friendly", TRUE);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Tabitha", 9, "M", "Black", "Viscious", TRUE);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Sergeant Brown", 2, "F", "Green", "Sadistic", TRUE);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Melissa", 5, "F", "Yellow", "Sadistic", TRUE);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Grinch", 9, "M", "Dark Brown", "Angry", TRUE);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Wendy", 3, "F", "Dark Brown", "Evil", TRUE);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, NULL, 20, "M", "Dark Brown", "Aggressive", TRUE);
